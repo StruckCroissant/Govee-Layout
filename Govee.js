af99603f7d710c7be3db6064de056998ab2f7034
Space Yeti Studios <jackson@spaceyetistudios.com>
2023-12-27 05:14:44 +0000

    Update file Govee.js

--- Govee.js
+++ Govee.js
@@ -157,21 +157,21 @@ function CreateLedMap(){
 //			ledNames.push(`Led ${i + 1}`);
 //		}
 //	}
-	else {
-		if (controller.sku == "H619B"){
-			ledPositions.push([3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [6,6], [6,7], [6,8], [5,8], [4,8], [3,8], [2,8], [1,8], [1,8], [1,7], [1,6], [1,6], [1,5], [1,4], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [3,0]);
-			for(let i = 0; i < ledCount; i++){
-				ledNames.push(`Led ${i + 1}`);
-			}
+//	else {
+	if (controller.sku == "H619B"){
+		ledPositions.push([3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [6,6], [6,7], [6,8], [5,8], [4,8], [3,8], [2,8], [1,8], [1,8], [1,7], [1,6], [1,6], [1,5], [1,4], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [3,0]);
+		for(let i = 0; i < ledCount; i++){
+			ledNames.push(`Led ${i + 1}`);
 		}
-		else {
-			for(let i = 0; i < ledCount; i++){
-				ledNames.push(`Led ${i + 1}`);
-				ledPositions.push([i, 0]);
-			}
+	}
+	else {
+		for(let i = 0; i < ledCount; i++){
+			ledNames.push(`Led ${i + 1}`);
+			ledPositions.push([i, 0]);
 		}
 	}
 }
+
 
 function ClearSubdevices(){
 	for(const subdevice of device.getCurrentSubdevices()){
