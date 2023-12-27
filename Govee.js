40ac79cb8c6e4ccb7b983731d88a8e74a22b277e
Space Yeti Studios <jackson@spaceyetistudios.com>
2023-12-27 05:19:40 +0000

    Update file Govee.js

--- Govee.js
+++ Govee.js
@@ -134,16 +134,11 @@ function SetLedCount(count){
 	ledCount = count;
 
 	CreateLedMap();
-	if (controller.sku == "H70B1"){
-		device.setSize([40, 40]);
-	}
+	if (controller.sku == "H619B"){
+		device.setSize([7, 9]);
+	}		
 	else {
-		if (controller.sku == "H619B"){
-			device.setSize([7, 9]);
-		}		
-		else {
-			device.setSize([ledCount, 1]);
-		}
+		device.setSize([ledCount, 1]);
 	}
 	device.setControllableLeds(ledNames, ledPositions);
 }
@@ -151,13 +146,6 @@ function SetLedCount(count){
 function CreateLedMap(){
 	ledNames = [];
 	ledPositions = [];
-//	if (controller.sku == "H70B1"){
-//		ledPositions.push([0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6], [0,7], [0,8], [0,9], [0,10], [0,11], [0,12], [0,13], [0,14], [0,15], [0,16], [0,17], [0,18], [0,19], [0,20], [0,21], [0,22], [0,23], [0,24], [0,25], [1,25], [1,24], [1,23], [1,22], [1,21], [1,20], [1,19], [1,18], [1,17], [1,16], [1,15], [1,14], [1,13], [1,12], [1,11], [1,10], [1,9], [1,8], [1,7], [1,6], [1,5], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [2,1], [2,2], [2,3], [2,4], [2,5], [2,6], [2,7]);
-//		for(let i = 0; i < ledCount; i++){
-//			ledNames.push(`Led ${i + 1}`);
-//		}
-//	}
-//	else {
 	if (controller.sku == "H619B"){
 		ledPositions.push([3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [6,6], [6,7], [6,8], [5,8], [4,8], [3,8], [2,8], [1,8], [1,8], [1,7], [1,6], [1,6], [1,5], [1,4], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [3,0]);
 		for(let i = 0; i < ledCount; i++){
@@ -654,7 +642,7 @@ const GoveeDeviceLibrary = {
 		state: 1,
 		supportRazer: true,
 		supportFeast: true,
-		ledCount: 60,
+		ledCount: 10,
 		hasVariableLedCount: true
 	}
 };
