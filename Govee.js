1465849654a5c02878511c0a1055ba32816480cd
Space Yeti Studios <jackson@spaceyetistudios.com>
2023-12-27 23:34:28 +0000

    Update file Govee.js

--- Govee.js
+++ Govee.js
@@ -134,32 +134,19 @@ function SetLedCount(count){
 	ledCount = count;
 
 	CreateLedMap();
-	if (controller.sku == "H619B"){
-		device.setSize([7, 9]);
-	}		
-	else {
-		device.setSize([ledCount, 1]);
-	}
+	device.setSize([ledCount, 1]);
 	device.setControllableLeds(ledNames, ledPositions);
 }
 
 function CreateLedMap(){
 	ledNames = [];
 	ledPositions = [];
-	if (controller.sku == "H619B"){
-		ledPositions.push([3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [6,6], [6,7], [6,8], [5,8], [4,8], [3,8], [2,8], [1,8], [1,8], [1,7], [1,6], [1,6], [1,5], [1,4], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [3,0]);
-		for(let i = 0; i < ledCount; i++){
-			ledNames.push(`Led ${i + 1}`);
-		}
-	}
-	else {
-		for(let i = 0; i < ledCount; i++){
-			ledNames.push(`Led ${i + 1}`);
-			ledPositions.push([i, 0]);
-		}
+
+	for(let i = 0; i < ledCount; i++){
+		ledNames.push(`Led ${i + 1}`);
+		ledPositions.push([i, 0]);
 	}
 }
-
 
 function ClearSubdevices(){
 	for(const subdevice of device.getCurrentSubdevices()){
@@ -399,7 +386,7 @@ const GoveeDeviceLibrary = {
 		state: 1,
 		supportRazer: true,
 		supportFeast: true,
-		ledCount: 30
+		ledCount: 15
 	},
 	H619C: {
 		productName: "10m RGBIC Pro Strip Lights",
@@ -643,7 +630,6 @@ const GoveeDeviceLibrary = {
 		supportRazer: true,
 		supportFeast: true,
 		ledCount: 10,
-		hasVariableLedCount: true
 	}
 };
 
