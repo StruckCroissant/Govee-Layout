57d0a471659fefb119b82b9aad5b112b317aab67
Space Yeti Studios <jackson@spaceyetistudios.com>
2023-12-27 01:08:13 +0000

    Update file Govee.js

--- Govee.js
+++ Govee.js
@@ -139,7 +139,7 @@ function SetLedCount(count){
 }
 
 function CreateLedMap(){
-	if (GoveeDeviceInfo.sku === "H961B") {
+	if (controller.sku === "H961B") {
 		let ledNames = [];
 		let ledPositions = [];
 		ledPositions.push([3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [4,5], [5,5], [6,5], [6,6], [6,7], [6,8], [5,8], [4,8], [3,8], [2,8], [1,8], [1,8], [1,7], [1,6], [1,6], [1,5], [1,4], [1,4], [1,3], [1,2], [1,1], [1,0], [2,0], [3,0])
