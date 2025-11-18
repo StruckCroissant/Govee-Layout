7b749b13cbe4aff4395ff8c0e42b03bbe42629d7
HarDBR <guicandido1337@hotmail.com>
2025-11-17 22:01:58 -0300

    H6048 mapping

--- Govee.js
+++ Govee.js
@@ -848,6 +848,32 @@ const GoveeDeviceLibrary = {
 		supportDreamView: true,
 		ledCount: 15
 	},
+	H6048: {
+		name: "RGBIC TV Light Bars Pro",
+		deviceImage: "https://assets.signalrgb.com/devices/brands/govee/wifi/h6048.png",
+		sku: "H6048",
+		state: 1,
+		supportRazer: true,
+		supportDreamView: true,
+		ledCount: 0,
+		usesSubDevices: true,
+		subdevices: [
+			{
+				name: "RGBIC TV Light Bars Pro",
+				ledCount: 10,
+				size: [1, 10],
+				ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
+				ledPositions: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9]],
+			},
+			{
+				name: "RGBIC TV Light Bars Pro",
+				ledCount: 10,
+				size: [1, 10],
+				ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
+				ledPositions: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9]],
+			},
+		]
+	},
 	H6051: {
 		name: "Table Lamp Lite",
 		deviceImage: "https://assets.signalrgb.com/devices/brands/govee/wifi/h6052.png",
